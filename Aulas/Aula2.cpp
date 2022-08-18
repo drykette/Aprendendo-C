2d79c327d582b0503409fa9a1ed1e99d4a09ac16
Adrielly Rodrigues <drykaa1@gmail.com>
2022-08-18 08:46:23 -0300

    Atualização do aprendizado

---
+++ Aulas/Aula2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Oi, tudo bom? \n");
+	printf("Valor inteiro: %d. \n", 10);
+	printf("Valor real: %f. \n", 3.14159265);
+	printf("Valor real com apenas duas casas: %.2f.\n", 3.14159265);
+	printf("Dado de texto: %c.\n", 'a');
+	printf("Dados de texto: %s.\n", "testando");
+	
+	
+	
+}
